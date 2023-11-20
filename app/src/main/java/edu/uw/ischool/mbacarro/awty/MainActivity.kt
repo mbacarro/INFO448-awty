c20fbbb40340f2855934ba56762bd28416005bcc
Matthew Bacarro <mbacarro@uw.edu>
2023-11-19 20:50:45 -0800

    part-2 assignment done

--- app/src/main/java/edu/uw/ischool/mbacarro/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/mbacarro/awty/MainActivity.kt
@@ -1,14 +1,18 @@
 package edu.uw.ischool.mbacarro.awty
 
-import android.content.Context
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +21,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var phoneNumberEditText: EditText
     private lateinit var intervalEditText: EditText
     private lateinit var startStopButton: Button
+    private lateinit var smsManager: SmsManager
 
     private val TAG = "MainActivity"
+    private val SMS_PERMISSION_REQUEST_CODE = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        smsManager = SmsManager.getDefault()
 
         messageEditText = findViewById(R.id.messageEditText)
         phoneNumberEditText = findViewById(R.id.phoneNumberEditText)
@@ -31,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 
         startStopButton.setOnClickListener {
             if (!isServiceRunning) {
-                startService()
+                checkAndRequestSMSPermission()
             } else {
                 stopService()
             }
@@ -50,6 +58,7 @@ class MainActivity : AppCompatActivity() {
                 val handler = Handler()
                 val runnable = object : Runnable {
                     override fun run() {
+                        sendSMS(phoneNumber, message)
                         showToast("$phoneNumber: $message")
                         handler.postDelayed(this, interval * 60 * 1000)
                     }
@@ -84,7 +93,7 @@ class MainActivity : AppCompatActivity() {
     private fun startService(message: String, phoneNumber: String, interval: Long) {
         Thread {
             while (isServiceRunning) {
-                sendToastMessage(message, phoneNumber)
+                sendSMS(phoneNumber, message)
                 try {
                     Thread.sleep(interval * 60 * 1000)
                 } catch (e: InterruptedException) {
@@ -94,10 +103,42 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
-    private fun sendToastMessage(message: String, phoneNumber: String) {
-        runOnUiThread {
-            showToast("$phoneNumber: $message")
+    private fun sendSMS(phoneNumber: String, message: String) {
+        try {
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+        } catch (e: Exception) {
+            Log.e(TAG, "Error sending SMS: ${e.message}")
+        }
+    }
+
+    private fun checkAndRequestSMSPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.SEND_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.SEND_SMS),
+                SMS_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            startService()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == SMS_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startService()
+            } else {
+                showToast("SMS permission denied. Cannot send messages.")
+            }
         }
     }
 }
-
